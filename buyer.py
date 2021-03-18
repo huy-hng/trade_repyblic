e20d5c6a19019342861b3b128513165a22b6f4aa
Huy <huy@hhoang.dev>
2021-03-18 14:13:21 +0100

    refactoring

---
+++ buyer.py
@@ -0,0 +1,82 @@
+class Buyer:
+	def __init__(self, starting_money, ohlc):
+		self.money = starting_money
+		self.shares = 0
+		self.transaction_cost = 1
+		self.transactions = []
+		self.ohlc = ohlc
+		self.data = df.copy()
+		# self.data['shares'] = 0
+		# self.data['money'] = 0
+		# self.data['equity'] = 0
+
+
+	def buy(self, row, money_to_buy):
+		price_per_share = row[self.ohlc]
+
+		if self.money - (money_to_buy + self.transaction_cost) < 0:
+			print(f'{row.name}, buy warning: not enough money')
+			return
+
+		self.shares += money_to_buy / price_per_share
+		self.money -= money_to_buy + self.transaction_cost
+
+		self.data.loc[row.name, 'shares'] = self.shares
+		self.data.loc[row.name, 'money'] = self.money
+		
+		self.transactions.append({
+			'date': row.name,
+			'price': round(price_per_share, 2),
+			'holding_shares': round(self.shares, 4),
+			'money': round(self.money, 2),
+			'share_transaction': round(money_to_buy / price_per_share, 4),
+		})
+
+	def sell(self, row, shares_to_sell):
+		price_per_share = row[self.ohlc]
+
+		if self.shares - shares_to_sell < 0:
+			print(f'{row.name}, sell warning: not enough shares')
+			return
+		if shares_to_sell * price_per_share - self.transaction_cost < 0:
+			print(f'{row.name}, sell warning: not enough money')
+			return
+
+		self.shares -= shares_to_sell
+		self.money += shares_to_sell * price_per_share - self.transaction_cost
+
+		self.data.loc[row.name, 'shares'] = self.shares
+		self.data.loc[row.name, 'money'] = self.money
+
+		self.transactions.append({
+			'date': row.name,
+			'price': round(price_per_share, 2),
+			'holding_shares': round(self.shares, 4),
+			'money': round(self.money, 2),
+			'share_transaction': round(-shares_to_sell, 4),
+		})
+
+	def equity(self):
+		# self.money + self.shares * shareprice
+		last_money = None
+		last_shares = None
+		for i, row in self.data.iterrows():
+
+			if row['money']	!= last_money and not math.isnan(row['money']):
+				last_money = row['money']
+			if math.isnan(row['money']):
+				self.data.loc[i, 'money'] = last_money
+
+			if row['shares']	!= last_shares and not math.isnan(row['shares']):
+				last_shares = row['shares']
+			if math.isnan(row['shares']):
+				self.data.loc[i, 'shares'] = last_shares
+
+		self.data['equity'] = self.data['shares'] * self.data[self.ohlc] + \
+													self.data['money']
+		self.data.dropna()
+
+
+
+	def is_holding(self):
+		return True if self.shares > 0 else False
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from IPython import get_ipython
 import os
 import datetime as dt
 import json
+import math
 
 import settings
 
@@ -23,15 +24,200 @@ get_ipython().run_line_magic('matplotlib', 'widget')
 OHLC = 'Close'
 RESAMPLE = '5D'
 
-# %%
+# delta = dt.timedelta(days=720)
+# end = dt.datetime(2015, 1, 1)
+# end = dt.datetime.now()
+# df = web.DataReader('SPY', 'yahoo', end-delta, end)
+# df.to_csv('prices/spy.csv')
+
 start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2021, 2, 26)
+end = dt.datetime.now()
 df = web.DataReader('SPY', 'yahoo', start, end)
 df.to_csv('prices/spy.csv')
-# df
 
+# for row in df.iterrows():
+# 	print(type(row[0]))
+# 	break
+
+# %% 
+#region helper functions
+def get_moving_average(OHLC, window):
+	return df[OHLC].rolling(window, min_periods=0).mean().values
+
+def calc_mav(arr, rolling_window=50):
+	vals = []
+	mav = []
+
+	for val in arr:
+		vals.append(val)
+
+		if len(vals) < rolling_window:
+			mav.append(sum(vals)/len(vals))
+		else:
+			mav.append(sum(vals[-rolling_window:])/rolling_window)
+
+	return mav
+
+def calc_current_mav(vals, window):
+	if len(vals) < window:
+		mav = sum(vals)/len(vals)
+	else:
+		mav = sum(vals[-window:])/window
+	return mav
+
+def print_endresult(starting_money, baseline_buyer, buyer, last_price):
+	buyer_profit = round(buyer.money + buyer.shares * last_price, 2)
+	baseline_profit = round(baseline_buyer.money + \
+													baseline_buyer.shares * last_price, 2)
+	print(f'Money at the end: {buyer.money},\
+					shares still left: {buyer.shares},\
+					last_price: {round(last_price, 2)}')
+	print(f'Money in total: {buyer_profit}')
+	print(f'Transaction costs: {len(buyer.transactions)}')
+
+	print(f'Baseline to beat: {baseline_profit}')
+	print(f'Improvement compared to baseline:\
+					{round(((buyer_profit / baseline_profit) -1) * 100, 2)}%')
+	print(f'Profit: {round(((buyer_profit / starting_money)-1) * 100, 2)}%')
+
+#endregion
+
+
+# %% ###########################################################################
+def golden_cross():
+	OHLC = 'Close'
+
+	money = 10000
+	buyer = Buyer(money, OHLC)
+	baseline_buyer = Buyer(money, OHLC)
+
+	mav_window_long = 200
+
+	mav_window_short = 50
+
+
+	prev_up = None
+	vals = []
+	for index, row in df.iterrows():
+		curr_price = row[OHLC]
+		
+		if not baseline_buyer.is_holding():
+			baseline_buyer.buy(row, baseline_buyer.money - 1)
+
+		vals.append(curr_price)
+
+		mav_long = calc_current_mav(vals, mav_window_long)
+		mav_short = calc_current_mav(vals, mav_window_short)
+
+		seven_day_low = min(vals[-7:])
+		seven_day_high = max(vals[-7:])
+
+		curr_up = 'long' if mav_long > mav_short else 'short'
+
+		if not buyer.is_holding():
+			if curr_up != prev_up and curr_up == 'short' and curr_price > mav_long:
+				buyer.buy(row, buyer.money - 1)
+		else:
+			if curr_up != prev_up and curr_up == 'long':
+				buyer.sell(row, buyer.shares)
+		prev_up = curr_up
+
+	print_endresult(money, baseline_buyer, buyer, calc_current_mav(vals, 30))
+
+
+	trans = buyer.transactions
+	buyer.equity()
+	# mpf.plot(buyer.data,
+	# 				type='candle',
+	# 				style='charles',
+	# 				addplot=mpf.make_addplot(buyer.data['equity'], type='line'))
+	return buyer
+# golden_cross()
+
+# %% ###########################################################################
+def mav_channel():
+	OHLC = 'Close'
+
+	money = 10000
+	buyer = Buyer(money, OHLC)
+	baseline_buyer = Buyer(money, OHLC)
+
+	mav_window_long = 200
+	mav_window_short = 20
+
+
+	prev_up = None
+	vals = []
+	lows = []
+	highs = []
+	bars_below_mac = 0
+	bars_above_mac = 0
+	for index, row in df.iterrows():
+		curr_price = row[OHLC]
+		
+		lows.append(row['Low'])
+		highs.append(row['High'])
+		vals.append(curr_price)
+
+		if not baseline_buyer.is_holding():
+			baseline_buyer.buy(row, baseline_buyer.money - 1)
+
+		mav = calc_current_mav(vals, 200)
+		mav_low = calc_current_mav(lows, 10)
+		mav_high = calc_current_mav(highs, 10)
+		if curr_price > mav_high:
+			bars_above_mac += 1
+			bars_below_mac = 0
+		elif curr_price < mav_low:
+			bars_below_mac += 1
+			bars_above_mac = 0
+		else:
+			bars_above_mac = 0
+			bars_below_mac = 0
+
+		if not buyer.is_holding():
+			if bars_above_mac >= 5 and curr_price > mav:
+				buyer.buy(row, buyer.money - 1)
+		else:
+			if bars_below_mac >= 5:
+				buyer.sell(row, buyer.shares)
+
+	print_endresult(money, baseline_buyer, buyer, calc_current_mav(vals, 30))
+
+	trans = buyer.transactions
+	buyer.equity()
+	return buyer
+
+
+
+
+
+
+
+# %% ###########################################################################
+################################################################################
+vals = []
+man_mav = []
+seven_day_lows = []
+seven_day_highs = []
+
+mav_window_long = 50
+for curr_price in df[OHLC].values:
+	vals.append(curr_price)
+
+	if len(vals) < mav_window_long:
+		man_mav.append(sum(vals)/len(vals))
+	else:
+		man_mav.append(sum(vals[-mav_window_long:])/mav_window_long)
+
+	seven_day_lows.append(min(vals[-7:]))
+	seven_day_highs.append(max(vals[-7:]))
+
+# %% ###########################################################################
+################################################################################
+buyer_gc = golden_cross()
+buyer_mac = mav_channel()
 # %%
-
 close = df['Close'].values
 high = df['High'].values
 low = df['Low'].values
@@ -55,73 +241,43 @@ for i, day in enumerate(zipped):
 
 df['atr'] = calc_mav(tr, 20)
 
-# %%
-def calc_mav(arr, rolling_window=50):
-	counter = 1
-	vals = []
-	mav = []
-
-	for val in arr:
-		vals.append(val)
-
-		if counter < rolling_window:
-			mav.append(sum(vals)/counter)
-		else:
-			mav.append(sum(vals[-rolling_window:])/rolling_window)
-
-		counter += 1
-
-	return mav
-
-# %%
-counter = 1
-vals = []
-man_mav = []
-seven_day_lows = []
-seven_day_highs = []
-
-rolling_window = 50
-for curr_price in df[OHLC].values:
-	vals.append(curr_price)
-
-	if counter < rolling_window:
-		man_mav.append(sum(vals)/counter)
-	else:
-		man_mav.append(sum(vals[-rolling_window:])/rolling_window)
-
-	seven_day_lows.append(min(vals[-7:]))
-	seven_day_highs.append(max(vals[-7:]))
-
-		
-	counter += 1
-
-
-# %%
-def get_moving_average(OHLC, window):
-	return df[OHLC].rolling(window, min_periods=0).mean().values
-
-
-
 mav_long = get_moving_average(OHLC, 200)
 mav_short = get_moving_average(OHLC, 20)
 
-
+crosses = pd.DataFrame(index=df.index, columns=['val', 'date'])
+count = 0
+prev_up = None
+for day in zip(mav_long, mav_short, df.index):
+	curr_up = 'long' if day[0] > day[1] else 'short'
+	if curr_up != prev_up:
+		crosses.loc[day[2], 'val'] = day[0] * 0.9
+		crosses.loc[day[2], 'date'] = day[2]
+	prev_up = curr_up
+	# if count == 10:
+	# 	break
+	# count += 1
 
 mavdf = pd.DataFrame({
-	# 'mav_long': mav_long,
-	'seven_day_lows': seven_day_lows,
-	'seven_day_highs': seven_day_highs,
-	'man_mav': man_mav,
+	'mav_long': mav_long,
+	'mav_short': mav_short,
+	# 'seven_day_lows': seven_day_lows,
+	# 'seven_day_highs': seven_day_highs,
+	# 'man_mav': man_mav,
 	}, index=df.index)
 
 # print(mavdf.resample('20D').ohlc())
 # ap = mpf.make_addplot(mavdf.resample(RESAMPLE).mean(), type='line')
 
-
-
+last_days = 0
 additional_plots = [
 	mpf.make_addplot(mavdf, type='line'),
-	mpf.make_addplot((df['atr']),panel=1,color='g',linestyle='dotted')
+	mpf.make_addplot((df['atr']), panel=1,color='g',linestyle='dotted'),
+	# mpf.make_addplot(crosses, type='scatter'),
+	# mpf.make_addplot(crosses, type='scatter', marker='^'),
+	# mpf.make_addplot(buyer_gc.data.iloc[last_days:, 'equity']),
+	# mpf.make_addplot(buyer_mac.data.iloc[last_days:, 'equity'])
+	mpf.make_addplot(buyer_gc.data['equity'], panel=2, color='blue'),
+	mpf.make_addplot(buyer_mac.data['equity'], panel=2, color='red')
 ]
 
 # mpf.plot(df[OHLC].resample(RESAMPLE).ohlc(),
@@ -130,212 +286,3 @@ mpf.plot(df,
         style='charles',
         addplot=additional_plots)
 
-# %%
-# region buyer
-class Buyer:
-	def __init__(self, starting_money, ohlc):
-		self.money = starting_money
-		self.shares = 0
-		self.transaction_cost = 1
-		self.transactions = []
-		self.ohlc = ohlc
-
-
-	def buy(self, row, money_to_buy):
-		price_per_share = row[self.ohlc]
-
-		if self.money - (money_to_buy + self.transaction_cost) < 0:
-			print(f'{row.name}, buy warning: not enough money')
-			return
-
-		self.shares += money_to_buy / price_per_share
-		self.money -= money_to_buy + self.transaction_cost
-
-		self.transactions.append({
-			'date': row.name,
-			'price': round(price_per_share, 2),
-			'holding_shares': round(self.shares, 4),
-			'money': round(self.money, 2),
-			'share_transaction': round(money_to_buy / price_per_share, 4),
-		})
-
-	def sell(self, row, shares_to_sell):
-		price_per_share = row[self.ohlc]
-
-		if self.shares - shares_to_sell < 0:
-			print(f'{row.name}, sell warning: not enough shares')
-			return
-		if shares_to_sell * price_per_share - self.transaction_cost < 0:
-			print(f'{row.name}, sell warning: not enough money')
-			return
-
-		self.shares -= shares_to_sell
-		self.money += shares_to_sell * price_per_share - self.transaction_cost
-
-		self.transactions.append({
-			'date': row.name,
-			'price': round(price_per_share, 2),
-			'holding_shares': round(self.shares, 4),
-			'money': round(self.money, 2),
-			'share_transaction': round(-shares_to_sell, 4),
-		})
-
-	def is_holding(self):
-		return True if self.shares > 0 else False
-# endregion
-
-# %%
-def print_endresult(starting_money, baseline_buyer, buyer, last_price):
-	buyer_profit = round(buyer.money + buyer.shares * last_price, 2)
-	baseline_profit = round(baseline_buyer.money + baseline_buyer.shares * last_price, 2)
-	print(f'Money at the end: {buyer.money}, shares still left: {buyer.shares}, last_price: {round(last_price, 2)}')
-	print(f'Money in total: {buyer_profit}')
-	print(f'Transaction costs: {len(buyer.transactions)}')
-
-	print(f'Baseline to beat: {baseline_profit}')
-	print(f'Improvement compared to baseline: {round(((buyer_profit / baseline_profit) -1) * 100, 2)}%')
-	print(f'Profit: {round(((buyer_profit / starting_money)-1) * 100, 2)}%')
-
-# %%
-# region 
-buyer = Buyer(1000)
-
-rolling_window = 200
-
-counter = 1
-vals = []
-last_price = None
-for index, row in df.iterrows():
-	# print(dir(row))
-	date: pd._libs.tslibs.timestamps.Timestamp = row.name
-
-	curr_price = row[OHLC]
-
-	vals.append(curr_price)
-
-	if counter < rolling_window:
-		current_mav = sum(vals)/counter
-	else:
-		current_mav = sum(vals[-rolling_window:])/rolling_window
-
-	counter += 1
-		
-	seven_day_low = min(vals[-7:])
-	seven_day_high = max(vals[-7:])
-
-
-	if buyer.is_holding():
-
-		if seven_day_low > current_mav:
-			if last_price > curr_price:
-				buyer.sell(curr_price, buyer.shares)
-	else:
-		if seven_day_high < current_mav:
-			if last_price < curr_price:
-				buyer.buy(curr_price, buyer.money * 0.8)
-				pass
-
-	last_price = curr_price
-
-# print(f'Money at the end: {buyer.money}, shares still left: {buyer.shares}, last_price: {last_price}')
-# total_money = buyer.money + buyer.shares * last_price
-# print(f'Money in total: {total_money}')
-# print(f'Transaction costs: {len(buyer.transactions)}')
-
-trans = buyer.transactions
-# endregion
-# %%
-# region 
-OHLC = 'Close'
-
-money = 10000
-buyer = Buyer(money, OHLC)
-baseline_buyer = Buyer(money, OHLC)
-
-rolling_window = 200
-
-counter = 1
-vals = []
-last_price = None
-for index, row in df.iterrows():
-	curr_price = row[OHLC]
-	
-	if not baseline_buyer.is_holding():
-		baseline_buyer.buy(row, baseline_buyer.money - 1)
-
-	vals.append(curr_price)
-
-	if counter < rolling_window:
-		current_mav = sum(vals)/counter
-	else:
-		current_mav = sum(vals[-rolling_window:])/rolling_window
-
-	counter += 1
-		
-	seven_day_low = min(vals[-7:])
-	seven_day_high = max(vals[-7:])
-
-
-	if not buyer.is_holding():
-		if curr_price < current_mav:
-			if row['Low'] < seven_day_low:
-				buyer.buy(row, buyer.money * 0.75)
-	else:
-		if curr_price < current_mav:
-			if row['High'] > seven_day_high:
-				buyer.sell(row, buyer.shares)
-
-	last_price = curr_price
-
-print_endresult(baseline_buyer, buyer, last_price)
-
-trans = buyer.transactions
-# endregion
-
-# %%
-# region
-OHLC = 'Close'
-starting_money = 10000
-buyer = Buyer(starting_money, OHLC)
-baseline_buyer = Buyer(starting_money, OHLC)
-
-rolling_window = 200
-
-counter = 1
-vals = []
-last_price = None
-for index, row in df.iterrows():
-	curr_price = row[OHLC]
-	
-	if not baseline_buyer.is_holding():
-		baseline_buyer.buy(row, baseline_buyer.money - 1)
-
-
-	vals.append(curr_price)
-
-	x_days = 5
-	x_day_low = min(vals[-x_days:])
-	x_day_high = max(vals[-x_days:])
-
-	if not buyer.is_holding():
-		if row['Low'] < x_day_low and buyer.money > 1:
-			buyer.buy(row, buyer.money - 1)
-	else:
-		if row['High'] > x_day_high and buyer.shares > 0:
-			buyer.sell(row, buyer.shares)
-	
-	# print(f'curr_price: {round(curr_price, 2)}')
-	# print(f'x_day_low: {round(x_day_low, 2)}')
-	# print(f'Low: {round(row["Low"], 2)}')
-	# print(f'x_day_high: {round(x_day_high, 2)}')
-	# print(f'High: {round(row["High"], 2)}')
-	# print(f'shares: {round(buyer.shares, 2)}')
-	# print()
-
-	last_price = curr_price
-	counter += 1
-
-print_endresult(starting_money, baseline_buyer, buyer, last_price)
-
-trans = buyer.transactions
-# endregion
